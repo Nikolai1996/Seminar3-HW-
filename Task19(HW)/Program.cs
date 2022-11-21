6504aa7819ebf56a17c2f2581b89a29441cd6a67
Nikolai <nikolay.golovachev96@mail.ru>
2022-11-21 18:31:32 +0300

    Add all HW

---
+++ Task19(HW)/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая 
+// 1. принимает на вход пятизначное число и 
+// 2. проверяет, является ли оно палиндромом.
+// Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+System.Console.WriteLine("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void Equality(int num)
+{
+       if(num > 10000 && num <100000) 
+    {
+        int one = num / 10000;
+        int two = num / 1000 % 10;
+        int three = num / 100 % 10;
+        int four = num / 10 % 10;
+        int five = num % 10;
+        if (one == five && two == four)
+        {
+             Console.WriteLine("Да");
+        }
+        else
+        {
+            Console.WriteLine("Нет");
+        }
+    }
+
+}
+
+Equality(number);
---
+++ Task23(HW)/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая 
+// 1. принимает на вход число (N) и 
+// 2. выдаёт таблицу кубов чисел от 1 до N.
+
+// 5 ->
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
+
+Console.WriteLine("Введите натуральное число ->");
+int number =Convert.ToInt32(Console.ReadLine());
+
+
+void Quad(int num)
+{
+int num1 = 1;
+while (num >= num1)
+{
+int square = num1 * num1 * num1;
+Console.WriteLine($"{num1, 4} {square, 4}");
+num1 ++;
+}
+}
+if (number > 0)
+Quad(number);
+else Console.WriteLine("Число не является натуральным");
